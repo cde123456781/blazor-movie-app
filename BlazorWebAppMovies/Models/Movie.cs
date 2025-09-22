e8162c462cd77eb4f7067168cbfcd1a7334287a6
cde123456781 <bjc199@uclive.ac.nz>
2025-09-23 02:27:04 +1200

    Finish Blazor movie app

---
+++ BlazorWebAppMovies/Models/Movie.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace BlazorWebAppMovies.Models
+{
+    public class Movie
+    {
+
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(60, MinimumLength =2)]
+        public string? Title { get; set; }
+
+        public DateOnly ReleaseDate { get; set; }
+
+        [Required]
+        [StringLength(30)]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z()\s-]*$")]
+        public string? Genre { get; set; }
+
+        [DataType(DataType.Currency)]
+        [Column(TypeName = "decimal(18, 2)")]
+        [Range(0, 100)]
+        public decimal Price { get; set; }
+
+        [Required]
+        [RegularExpression(@"^(G|PG|PG-13|R|NC-17)$")]
+        public string? Rating { get; set; }
+    }
+}
--- BlazorWebAppMovies/Program.cs
+++ BlazorWebAppMovies/Program.cs
@@ -1,6 +1,15 @@
 using BlazorWebAppMovies.Components;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using BlazorWebAppMovies.Data;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContextFactory<BlazorWebAppMoviesContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("BlazorWebAppMoviesContext") ?? throw new InvalidOperationException("Connection string 'BlazorWebAppMoviesContext' not found.")));
+
+builder.Services.AddQuickGridEntityFrameworkAdapter();
+
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
@@ -8,13 +17,21 @@ builder.Services.AddRazorComponents()
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+using (var scope = app.Services.CreateScope())
 {
-    app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    var services = scope.ServiceProvider;
+
+    SeedData.Initialize(services);
 }
+
+    // Configure the HTTP request pipeline.
+    if (!app.Environment.IsDevelopment())
+    {
+        app.UseExceptionHandler("/Error", createScopeForErrors: true);
+        // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+        app.UseHsts();
+        app.UseMigrationsEndPoint();
+    }
 
 app.UseHttpsRedirection();
 
